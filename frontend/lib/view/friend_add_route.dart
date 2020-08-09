466ccc12b179e8e6b705aeeb54316006147797c2
29maron <maron.panic@gmail.com>
2020-08-09 14:38:09 +0900

    add friend

--- frontend/lib/view/friend_add_route.dart
+++ frontend/lib/view/friend_add_route.dart
@@ -7,7 +7,54 @@ class FriendAdd extends StatelessWidget {
       appBar: AppBar(
         title: Text("友達追加"),
       ),
-      body: Center(child: Text("友達追加")),
+      body: Center(
+        child: Column(children: <Widget>[
+          SpaceBox.height(30),
+          Container(
+            width:MediaQuery.of(context).size.width * 0.8,
+            child:TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: '友達ID',
+                ),
+            ),
+          ),
+          SpaceBox.height(10),
+          Text('名前',
+              style: TextStyle(
+                fontSize: MediaQuery.of(context).size.width * 0.1,
+              ),
+          ),
+          SpaceBox.height(10),
+          Image.asset(
+            'images/aristoteres.png',
+            width: MediaQuery.of(context).size.width * 0.45,
+            height:MediaQuery.of(context).size.width * 0.45,
+          ),
+          SpaceBox.height(40),
+          ButtonTheme(
+            minWidth: MediaQuery.of(context).size.width * 0.6,
+            height: 50,
+            child:RaisedButton(
+              child: Text("友達申請",style: TextStyle(fontSize: 20)),
+              color: Colors.white,
+              shape: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+              ),
+              onPressed: () {},
+            ),
+          ),
+        ]),
+      ),
     );
   }
+}
+
+
+class SpaceBox extends SizedBox {
+  SpaceBox({double width = 8, double height = 8})
+      : super(width: width, height: height);
+
+  SpaceBox.width([double value = 8]) : super(width: value);
+  SpaceBox.height([double value = 8]) : super(height: value);
 }
--- frontend/lib/view/profile_route.dart
+++ frontend/lib/view/profile_route.dart
@@ -9,20 +9,43 @@ class Profile extends StatelessWidget {
       ),
       body: Center(
         child: Column(children: <Widget>[
+          SpaceBox.height(10),
           Text('名前',
               style: TextStyle(
-                fontSize: 40,
+                fontSize: MediaQuery.of(context).size.width * 0.1,
               )),
-          Image.asset('images/aristoteres.png'),
+          SpaceBox.height(10),
+          Image.asset(
+            'images/aristoteres.png',
+            width: MediaQuery.of(context).size.width * 0.45,
+            height:MediaQuery.of(context).size.width * 0.45,
+          ),
+          SpaceBox.height(30),
           Container(
-            color: Colors.blue[600],
-            child: Column(children: <Widget>[
-              Text('誕生日'),
-              Text('欲しいものリスト'),
-            ]),
+            width: MediaQuery.of(context).size.width * 0.8,
+            height: MediaQuery.of(context).size.width * 0.6,
+            padding: EdgeInsets.all(20),
+            color: Colors.grey[300],
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    '誕生日:',
+                    style: TextStyle(fontSize: MediaQuery.of(context).size.width * 0.05),
+                  ),
+                  Text('欲しいものリスト:', style: TextStyle(fontSize: MediaQuery.of(context).size.width * 0.05)),
+                ]),
           ),
         ]),
       ),
     );
   }
+}
+
+class SpaceBox extends SizedBox {
+  SpaceBox({double width = 8, double height = 8})
+      : super(width: width, height: height);
+
+  SpaceBox.width([double value = 8]) : super(width: value);
+  SpaceBox.height([double value = 8]) : super(height: value);
 }
