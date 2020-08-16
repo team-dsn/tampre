a22881666a2a87b22965a99d604c17e1c11e92b6
Hiroki Sawa <hsawa0709@gmail.com>
2020-08-16 18:54:29 +0900

    created user APIs

--- backend/containers/django_rest_api/tampre_app/webapi/urls.py
+++ backend/containers/django_rest_api/tampre_app/webapi/urls.py
@@ -4,5 +4,7 @@ from django.http import HttpResponse
 urlpatterns = [
     path('api/user_info/', views.UserInfoCreate.as_view() ),
     path('api/friend_info/', views.FriendInfoCreate.as_view() ),
-    path('api/healthcheck', lambda r: HttpResponse()),
+    path('api/healthcheck', lambda r: HttpResponse() ),
+    path('api/user/<str:user_id>', views.UserInfoAPIView.as_view() ),
+    path('api/user', views.UserInfoAPIView.as_view() ),
 ]
--- backend/containers/django_rest_api/tampre_app/webapi/views.py
+++ backend/containers/django_rest_api/tampre_app/webapi/views.py
@@ -1,6 +1,8 @@
 from webapi.models import UserInfo, FriendInfo
 from webapi.serializers import UserInfoSerializer, FriendInfoSerializer
-from rest_framework import generics
+from rest_framework import generics, status, views
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 class UserInfoCreate(generics.ListCreateAPIView):
     queryset = UserInfo.objects.all()
@@ -9,3 +11,47 @@ class UserInfoCreate(generics.ListCreateAPIView):
 class FriendInfoCreate(generics.ListCreateAPIView):
     queryset = FriendInfo.objects.all()
     serializer_class = FriendInfoSerializer
+
+class UserInfoAPIView(views.APIView):
+    """ ユーザモデルの登録、修正、削除 """
+
+    def get(self, request, user_id, *args, **kwargs):
+        """ ユーザモデル取得APIに対応するハンドラメソッド """
+
+        # モデルオブジェクトを取得
+        user = get_object_or_404(UserInfo, user_id=user_id)
+        # シリアライザオブジェクトを作成
+        serializer = UserInfoSerializer(instance=user)
+        # レスポンスオブジェクトを作成して返す
+        return Response(serializer.data, status.HTTP_200_OK)
+        
+    def post(self, request, *args, **kwargs):
+        """ ユーザモデル登録APIに対応するハンドラメソッド """
+        # シリアライズオブジェクトを作成
+        serializer = UserInfoSerializer(data=request.data)
+        # バリデーションを実行
+        serializer.is_valid(raise_exception=True)
+        # モデルオブジェクトを登録
+        serializer.save()
+        # レスポンスオブジェクトを作成して返す
+        return Response(serializer.data, status.HTTP_201_CREATED)
+
+    def put(self, request, user_id, *args, **kwargs):
+        """ ユーザモデル修正APIに対応するハンドラメソッド """
+        # モデルオブジェクトを取得
+        user = get_object_or_404(UserInfo, user_id=user_id)
+        # 各値をupdateする
+        user.user_name = request.data['user_name']
+        user.birthday = request.data['birthday']
+        user.profile_image_url = request.data['profile_image_url']
+        user.wish_list_url = request.data['wish_list_url']
+        # DBにsaveする
+        user.save()
+        # レスポンスオブジェクトを作成して返す
+        return Response(UserInfoSerializer(instance=user).data, status.HTTP_200_OK)
+
+    def delete(self, request, user_id, *args, **kwargs):
+        """ ユーザモデル削除APIに対応するハンドラメソッド """
+        # モデルオブジェクトを取得
+        user = get_object_or_404(UserInfo, user_id=user_id).delete()
+        return Response(status.HTTP_200_OK)
