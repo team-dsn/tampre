a3c3b3074dcd099cd176f4c65d348117ba639402
29maron <maron.panic@gmail.com>
2020-08-16 19:39:14 +0900

    [TAMPRE-30] 欲しいものリストへのリンク

--- frontend/lib/model/home_model.dart
+++ frontend/lib/model/home_model.dart
@@ -3,18 +3,18 @@ import 'package:tampre/view/component/user.dart';
 
 class HomeModel extends ChangeNotifier {
   List<User> users = [
-    User(id: '1', userName: 'アリストテレス', birthday: '19970125', icon: 'images/aristoteres.png'),
-    User(id: '2', icon: 'images/newton.jpeg', userName: 'ニュートン', birthday: '19921221'),
-    User(id: '3', icon: 'images/newton.jpeg', userName: 'ニューロン', birthday: '20000211'),
-    User(id: '4', icon: 'images/newton.jpeg', userName: 'ニューコメン', birthday: '20000311'),
-    User(id: '5', icon: 'images/newton.jpeg', userName: 'ニューごめん', birthday: '20000411'),
-    User(id: '6', icon: 'images/newton.jpeg', userName: 'ニューごめん', birthday: '20000511'),
-    User(id: '7', icon: 'images/newton.jpeg', userName: 'ニューごめん', birthday: '20000611'),
-    User(id: '8', icon: 'images/newton.jpeg', userName: 'ニューごめん', birthday: '20000711'),
-    User(id: '9', icon: 'images/newton.jpeg', userName: 'ニューごめん', birthday: '20000811'),
-    User(id: '10', icon: 'images/newton.jpeg', userName: 'ニューごめん', birthday: '20000911'),
-    User(id: '11', icon: 'images/newton.jpeg', userName: 'ニューごめん', birthday: '20001011'),
-    User(id: '12', icon: 'images/newton.jpeg', userName: 'ニューごめん', birthday: '20001111'),
+    User(id: '1', userName: 'アリストテレス', birthday: '19970125', icon: 'images/aristoteres.png', wishList: 'https://ja.wikipedia.org/wiki/newton'),
+    User(id: '2', icon: 'images/newton.jpeg', userName: 'ニュートン', birthday: '19921221', wishList: 'https://ja.wikipedia.org/wiki/newton'),
+    User(id: '3', icon: 'images/newton.jpeg', userName: 'ニューロン', birthday: '20000211', wishList: 'https://ja.wikipedia.org/wiki/newton'),
+    User(id: '4', icon: 'images/newton.jpeg', userName: 'ニューコメン', birthday: '20000311', wishList: 'https://ja.wikipedia.org/wiki/newton'),
+    User(id: '5', icon: 'images/newton.jpeg', userName: 'ニューごめん', birthday: '20000411', wishList: 'https://ja.wikipedia.org/wiki/newton'),
+    User(id: '6', icon: 'images/newton.jpeg', userName: 'ニューごめん', birthday: '20000511', wishList: 'https://ja.wikipedia.org/wiki/newton'),
+    User(id: '7', icon: 'images/newton.jpeg', userName: 'ニューごめん', birthday: '20000611', wishList: 'https://ja.wikipedia.org/wiki/newton'),
+    User(id: '8', icon: 'images/newton.jpeg', userName: 'ニューごめん', birthday: '20000711', wishList: 'https://ja.wikipedia.org/wiki/newton'),
+    User(id: '9', icon: 'images/newton.jpeg', userName: 'ニューごめん', birthday: '20000811', wishList: 'https://ja.wikipedia.org/wiki/newton'),
+    User(id: '10', icon: 'images/newton.jpeg', userName: 'ニューごめん', birthday: '20000911', wishList: 'https://ja.wikipedia.org/wiki/newton'),
+    User(id: '11', icon: 'images/newton.jpeg', userName: 'ニューごめん', birthday: '20001011', wishList: 'https://ja.wikipedia.org/wiki/newton'),
+    User(id: '12', icon: 'images/newton.jpeg', userName: 'ニューごめん', birthday: '20001111', wishList: 'https://ja.wikipedia.org/wiki/newton'),
   ];
 
   sort() {
