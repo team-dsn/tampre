a31991ca2e6f68e1cdbdc0a766bd757d1875b809
Hiroki Sawa <hsawa0709@gmail.com>
2020-08-29 14:24:03 +0900

    commit till last time

--- backend/containers/django_rest_api/tampre_app/webapi/urls.py
+++ backend/containers/django_rest_api/tampre_app/webapi/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('api/healthcheck', lambda r: HttpResponse() ),
     path('api/user/<str:user_id>', views.UserInfoAPIView.as_view() ),
     path('api/user', views.UserInfoAPIView.as_view() ),
-    path('api/friends/request/<str:user_id>/<str:friend_id>', views.UserFriendRequest.as_view() ),
+    path('api/friends/<str:user_id>', views.FriendListAPIView.as_view() ),
+    path('api/friends/<str:user_id>/<str:friend_id>', views.FriendDeleteAPIView.as_view() ),
+    path('api/friends/request/<str:user_id>/<str:friend_id>', views.FriendRequestAPIView.as_view() ),
+    path('api/friends/requested/<str:user_id>', views.FriendRequestedAPIView.as_view() ),
 ]
--- backend/containers/django_rest_api/tampre_app/webapi/views.py
+++ backend/containers/django_rest_api/tampre_app/webapi/views.py
@@ -50,17 +50,64 @@ class UserInfoAPIView(views.APIView):
     def delete(self, request, user_id, *args, **kwargs):
         """ ユーザモデル削除APIに対応するハンドラメソッド """
         # モデルオブジェクトを取得
-        user = get_object_or_404(UserInfo, user_id=user_id).delete()
-        return Response(status.HTTP_200_OK)
+        get_object_or_404(UserInfo, user_id=user_id).delete()
+        return Response(status.HTTP_204_NO_CONTENT)
 
-class FriendInfoAPIView(views.APIView):
+class FriendListAPIView(views.APIView):
     """ 友達情報を取得する """
     def get(self, request, user_id, *args, **kwargs):
         """ 友達情報取得APIに対応するハンドラメソッド """
-        pass
         # モデルオブジェクトを取得
-        #friends = FriendInfo.objects.filter(user_id=user_id, status=1)
+        friends = FriendInfo.objects.filter(user_id=user_id, status=0)
         # シリアライザオブジェクトを作成
-        # serializer = FriendInfoSerializer(instance=friends)
+        serializer = FriendInfoSerializer(instance=friends, many=True)
+        # 友達情報をUserInfoDBから取得
+        friend_list = []
+        for friend in serializer.data:
+            friend_list.append(UserInfo.objects.get(user_id=friend['friend_id']))
+        # シリアライザオブジェクトを作成  
+        serializer2 = UserInfoSerializer(instance=friend_list, many=True)
         # レスポンスオブジェクトを作成して返す
-        # return Response(serializer.data, status.HTTP_200_OK)
+        return Response(serializer2.data, status.HTTP_200_OK)
+
+class FriendDeleteAPIView(views.APIView):
+    """ 友達を削除する """
+    def delete(self, request, user_id, friend_id, *args, **kwargs):
+        # モデルオブジェクトを取得
+        friend = get_object_or_404(FriendInfo, user_id=user_id, friend_id=friend_id)     
+        # updateする
+        data = {'status': 2}
+        serializer = FriendInfoSerializer(instance=friend, data=data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        # レスポンスオブジェクトを作成して返す
+        return Response(serializer.data, status.HTTP_200_OK)
+
+class FriendRequestAPIView(views.APIView):
+    """ 友達申請を送る """
+    def post(self, request, user_id, friend_id, *args, **kwargs):
+        # TODO: バリデーション：Userがいない場合を考慮する
+        # シリアライズオブジェクトを作成
+        serializer = FriendInfoSerializer(data={"user_id":user_id, "friend_id":friend_id, "status":0})
+        # バリデーションを実行
+        serializer.is_valid(raise_exception=True)
+        # モデルオブジェクトを登録
+        serializer.save()
+        # レスポンスオブジェクトを作成して返す
+        return Response(serializer.data, status.HTTP_201_CREATED)
+
+class FriendRequestedAPIView(views.APIView):
+    """ 自分に来ている友達申請を確認する """
+    def get(self, request, user_id, *args, **kwargs):
+        # モデルオブジェクトを取得
+        friends = FriendInfo.objects.filter(friend_id=user_id, status=1)
+        # シリアライザオブジェクトを作成
+        serializer = FriendInfoSerializer(instance=friends, many=True)
+        # 友達情報をUserInfoDBから取得
+        friend_list = []
+        for friend in serializer.data:
+            friend_list.append(UserInfo.objects.get(user_id=friend['user_id']))
+        # シリアライザオブジェクトを作成  
+        serializer2 = UserInfoSerializer(instance=friend_list, many=True)
+        # レスポンスオブジェクトを作成して返す
+        return Response(serializer2.data, status.HTTP_200_OK)
