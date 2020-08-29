d7094876a0d659c2c0d385e058d5722909f1d6f0
Hiroki Sawa <hsawa0709@gmail.com>
2020-08-29 18:53:58 +0900

    modify response body

--- backend/containers/django_rest_api/tampre_app/webapi/serializers.py
+++ backend/containers/django_rest_api/tampre_app/webapi/serializers.py
@@ -4,7 +4,12 @@ from webapi.models import UserInfo, FriendInfo
 class UserInfoSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserInfo
-        fields = ('userId', 'userName', 'birthday', 'profileImageUrl', 'wishListUrl', 'createdAt')
+        fields = ('userId', 'userName', 'birthday', 'profileImageUrl', 'wishListUrl')
+
+class UserInfoSerializerForFriend(serializers.ModelSerializer):
+    class Meta:
+        model = UserInfo
+        fields = ('userId', 'userName', 'profileImageUrl')
 
 class FriendInfoSerializer(serializers.ModelSerializer):
     class Meta:
