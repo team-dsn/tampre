f9980293a7e0839921bb8b8ccf1c37e586a7e87a
narugit <narusens@gmail.com>
2020-09-12 16:46:56 +0900

    [TAMPRE-27] Do not scroll ListTile

--- frontend/lib/view/friend_add_route.dart
+++ frontend/lib/view/friend_add_route.dart
@@ -20,131 +20,138 @@ class FriendAdd extends StatelessWidget {
         ),
         body: Consumer<FriendAddModel>(builder: (context, model, child) {
             return Center(
-              child: Column(children: <Widget>[
-                RichText(
-                  text: TextSpan(
-                    text: '友達リクエスト',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: MediaQuery.of(context).size.width * 0.05,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  )
-                ),
-                Expanded(
-                  child: ListView(
-                    children: global.friendCandidates.map((friendCandidate) =>
-                      ListTile(
-                        leading: CircleAvatar(
-                          backgroundImage: AssetImage(friendCandidate.icon),
-                        ),
-                        title: Text(friendCandidate.userName),
-                        trailing: Row(
-                          children: <Widget>[
-                            RaisedButton(
-                              child: Text("承認"),
-                              color: Colors.blueAccent,
-                              textColor: Colors.white,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10.0),
-                              ),
-                              onPressed: () {},
-                            ),
-                            RaisedButton(
-                              child: Text("削除"),
-                              color: Colors.grey[300],
-                              textColor: Colors.black,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10.0),
-                              ),
-                              onPressed: () {},
-                            ),
-                          ],
-                          mainAxisSize: MainAxisSize.min,
-                        ),
-                      )
-                    ).toList(),
-                  ),
-                ),
-                Row(children: <Widget>[
-                  Spacer(),
-                  Container(
-                    width:MediaQuery.of(context).size.width * 0.6,
-                    height: 50,
-                    child:TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: '友達ID',
+              child: SingleChildScrollView(
+                child: Column(children: <Widget>[
+                  RichText(
+                    text: TextSpan(
+                      text: '友達リクエスト',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: MediaQuery.of(context).size.width * 0.05,
+                        fontWeight: FontWeight.bold,
                       ),
-                  onChanged: (text){
-                    id = text;
-                    print(id);
-                  }
+                    )
                   ),
-                ),
-                  Spacer(),
-                  ButtonTheme(
-                    minWidth: MediaQuery.of(context).size.width * 0.1,
-                    height: 50,
-                    child:RaisedButton(
-                      child: Icon(Icons.search),
-                      color: Colors.grey[300],
-                      shape:OutlineInputBorder(),
-                      onPressed: () {
-                        this.user = model.onSearchPressed(context, id);
-                      },
-                  )
-                ),
-                  Spacer(),
-              ],),
-                SizedBox(
-                height: 30,
-                ),
-                Text(user != null
-                  ? user.userName
-                  : '',
-                  style: TextStyle(
-                    fontSize: MediaQuery.of(context).size.width * 0.1,
+                  Container(
+                    height: 60.0 * global.friendRequestNumber, // 60.0はListTileを覆っているContainerのheightと揃える
+                    child: ListView(
+                      physics: NeverScrollableScrollPhysics(),
+                      children: global.friendCandidates.map((friendCandidate) =>
+                        Container(
+                          height: 60.0,
+                          child: ListTile(
+                            leading: CircleAvatar(
+                              backgroundImage: AssetImage(friendCandidate.icon),
+                            ),
+                            title: Text(friendCandidate.userName),
+                            trailing: Row(
+                              children: <Widget>[
+                                RaisedButton(
+                                  child: Text("承認"),
+                                  color: Colors.blueAccent,
+                                  textColor: Colors.white,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                  ),
+                                  onPressed: () {},
+                                ),
+                                RaisedButton(
+                                  child: Text("削除"),
+                                  color: Colors.grey[300],
+                                  textColor: Colors.black,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                  ),
+                                  onPressed: () {},
+                                ),
+                              ],
+                              mainAxisSize: MainAxisSize.min,
+                            ),
+                          )
+                        )
+                      ).toList(),
+                    ),
                   ),
-              ),
-                SizedBox(
-                height: 10,
-              ),
-                user != null
-                ? Image.asset(
-                  user.icon,
-                    width: MediaQuery.of(context).size.width * 0.5,
-                    height:MediaQuery.of(context).size.width * 0.5,
-                )
-                : Container(
-                    decoration: BoxDecoration(
-                      shape:BoxShape.circle,
-                      color: Colors.grey,
-                    ),
-                    child: Icon(Icons.person,
-                      color: Colors.white,
-                      size:MediaQuery.of(context).size.width * 0.5
-                    ),
-                ),
-
-                SizedBox(
-                height: 40,
-              ),
-                ButtonTheme(
-                minWidth: MediaQuery.of(context).size.width * 0.6,
-                height: 50,
-                  child: user != null
-                      ? RaisedButton(
-                          child: Text("友達申請",style: TextStyle(fontSize: 20)),
-                          color: Colors.white,
-                          shape: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  Row(children: <Widget>[
+                    Spacer(),
+                    Container(
+                      width:MediaQuery.of(context).size.width * 0.6,
+                      height: 50,
+                      child:TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: '友達ID',
                         ),
-                        onPressed: () {},
-                      )
-                      : Container(),
-              ),
-            ]),
+                    onChanged: (text){
+                      id = text;
+                      print(id);
+                    }
+                    ),
+                  ),
+                    Spacer(),
+                    ButtonTheme(
+                      minWidth: MediaQuery.of(context).size.width * 0.1,
+                      height: 50,
+                      child:RaisedButton(
+                        child: Icon(Icons.search),
+                        color: Colors.grey[300],
+                        shape:OutlineInputBorder(),
+                        onPressed: () {
+                          this.user = model.onSearchPressed(context, id);
+                        },
+                    )
+                  ),
+                    Spacer(),
+                ],),
+                  SizedBox(
+                  height: 30,
+                  ),
+                  Text(user != null
+                    ? user.userName
+                    : '',
+                    style: TextStyle(
+                      fontSize: MediaQuery.of(context).size.width * 0.1,
+                    ),
+                ),
+                  SizedBox(
+                  height: 10,
+                ),
+                  user != null
+                  ? Image.asset(
+                    user.icon,
+                      width: MediaQuery.of(context).size.width * 0.5,
+                      height:MediaQuery.of(context).size.width * 0.5,
+                  )
+                  : Container(
+                      decoration: BoxDecoration(
+                        shape:BoxShape.circle,
+                        color: Colors.grey,
+                      ),
+                      child: Icon(Icons.person,
+                        color: Colors.white,
+                        size:MediaQuery.of(context).size.width * 0.5
+                      ),
+                  ),
+  
+                  SizedBox(
+                  height: 40,
+                ),
+                  ButtonTheme(
+                  minWidth: MediaQuery.of(context).size.width * 0.6,
+                  height: 50,
+                    child: user != null
+                        ? RaisedButton(
+                            child: Text("友達申請",style: TextStyle(fontSize: 20)),
+                            color: Colors.white,
+                            shape: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                          ),
+                          onPressed: () {},
+                        )
+                        : Container(),
+                ),
+              ]),
+            )
           );
         }),
       )
