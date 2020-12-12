fbed3c9dc62dd0a7f9b508e8861a8549fdcca512
Hiroki Sawa <hsawa0709@gmail.com>
2020-12-12 15:59:56 +0900

    nest birthday params (#68)

--- backend/containers/django_rest_api/tampre_app/webapi/models.py
+++ backend/containers/django_rest_api/tampre_app/webapi/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 class UserInfo(models.Model):
     """ ユーザ情報モデル """
-
     userId = models.CharField(max_length=64, unique=True)
     userName = models.CharField(max_length=64)
-    birthday = models.CharField(max_length=8)
+    birthday_year = models.CharField(max_length=4)
+    birthday_month = models.CharField(max_length=2)
+    birthday_day = models.CharField(max_length=2)
     profileImageUrl = models.CharField(max_length=256)
     wishListUrl = models.CharField(max_length=256)
     createdAt = models.DateTimeField(auto_now_add=True)
