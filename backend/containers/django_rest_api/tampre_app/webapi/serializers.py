652a20bf2849a810ec0c1d29fc8ad78ca77347c4
Hiroki Sawa <hsawa0709@gmail.com>
2020-12-12 15:59:12 +0900

    nest birthday params

--- backend/containers/django_rest_api/tampre_app/webapi/serializers.py
+++ backend/containers/django_rest_api/tampre_app/webapi/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from webapi.models import UserInfo, FriendInfo
 
+class BirthdaySerializer(serializers.Serializer):
+    year = serializers.CharField(max_length=4, source='birthday_year')
+    month = serializers.CharField(max_length=2, source='birthday_month')
+    day = serializers.CharField(max_length=2, source='birthday_day')
+
 class UserInfoSerializer(serializers.ModelSerializer):
+    birthday = BirthdaySerializer(source='*')
+
     class Meta:
         model = UserInfo
         fields = ('userId', 'userName', 'birthday', 'profileImageUrl', 'wishListUrl')
